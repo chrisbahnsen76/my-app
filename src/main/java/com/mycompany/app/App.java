7a23a33f1bca308a653bc3a770f1daf6d5bf6548
root <root@localhost.localdomain>
2019-07-25 14:12:17 -0400

    i added a comment

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,6 +6,7 @@ package com.mycompany.app;
  */
 public class App 
 {
+    // this is the main method right here!
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
